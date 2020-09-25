4c8f3df37630823148e12bf05ced3287c000f8ec
Christina Song <christina.song202@gmail.com>
2020-09-25 17:46:55 -0400

    updated the js functions, img functions still don't work

---
+++ myScript.js
@@ -0,0 +1,6 @@
+function imgOrange() {
+    document.getElementById("imgFruit").src="images/orange.jpg";
+}
+function imgApple() {
+    document.getElementById("imgApple").src="images/apple.jpg";
+}
