d187949cecc3e03133af121cfc7c76b74e05015b
Christina Song <christina.song202@gmail.com>
2020-09-25 17:58:54 -0400

    finally fixed the stupid functions

--- myScript.js
+++ myScript.js
@@ -2,5 +2,5 @@ function imgOrange() {
     document.getElementById("imgFruit").src="images/orange.jpg";
 }
 function imgApple() {
-    document.getElementById("imgApple").src="images/apple.jpg";
+    document.getElementById("imgFruit").src="images/apple.jpg";
 }
